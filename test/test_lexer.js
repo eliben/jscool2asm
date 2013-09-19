0165f2d492b13afb455ff8f194344890bc266a1b
Eli Bendersky <eliben@gmail.com>
2013-09-19 06:02:11 -0700

    rewrote assert_lexer_tokens to be permissive - allow not specifying .pos and .lineno in the expected tokens

--- test/test_lexer.js
+++ test/test_lexer.js
@@ -24,10 +24,27 @@ var lex_all = function(str) {
 
 
 // Assert that lexing str produces the expected tokens, without any errors.
+// The expected tokens is an array of objects. These objects must have .name
+// and .value attributes. Optionally, they can also have .lineno and .pos - if
+// these exist they will be compared to the actual tokens.
 var assert_lexer_tokens = function(str, expected_toks) {
   var result = lex_all(str);
+  assert.equal(result.tokens.length, expected_toks.length);
 
-  assert.deepEqual(result.tokens, expected_toks);
+  for (var i = 0; i < result.tokens.length; ++i) {
+    var exp_tok = expected_toks[i];
+    var result_tok = result.tokens[i];
+
+    assert.equal(exp_tok.name, result_tok.name);
+    assert.equal(exp_tok.value, result_tok.value);
+    if (exp_tok.hasOwnProperty('pos')) {
+      assert.equal(exp_tok.pos, result_tok.pos);
+    }
+    if (exp_tok.hasOwnProperty('lineno')) {
+      assert.equal(exp_tok.lineno, result_tok.lineno);
+    }
+  }
+
   assert.deepEqual(result.errors, []);
 }
 
@@ -42,8 +59,8 @@ var assert_lexer_errors = function(str, expected_errors) {
 var test = function() {
   // Basic tokens
   assert_lexer_tokens('foobar 123', [
-    {"name":"IDENTIFIER","value":"foobar","pos":0,"lineno":1},
-    {"name":"NUMBER","value":"123","pos":7,"lineno":1}
+    {"name":"IDENTIFIER","value":"foobar"},
+    {"name":"NUMBER","value":"123"}
     ]);
   assert_lexer_errors('topo %', ["Line 1: Unknown token '%'"]);
   assert_lexer_errors('topo \n$', ["Line 2: Unknown token '$'"]);
