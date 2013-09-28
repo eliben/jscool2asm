5f41f45f0c2f3a8cb59468e6f1b906dfaf354a7f
Eli Bendersky <eliben@gmail.com>
2013-09-28 11:25:08 -0700

    cleanup comments and add AST skeleton from asdl.py

--- tools/asdl_parser.py
+++ tools/asdl_parser.py
@@ -45,8 +45,8 @@ def tokenize_asdl(buf):
                 yield Token(TokenKind.TypeId, id, lineno)
             pos = end
         elif c == '-':
+            # Potential comment, if followed by another '-'
             if pos < buflen - 1 and buf[pos + 1] == '-':
-                # Comment. Skip until newline.
                 pos = buf.find('\n', pos + 1)
                 if pos < 0: pos = buflen
                 continue
@@ -61,6 +61,82 @@ def tokenize_asdl(buf):
                 raise TokenError('Invalid operator %s on line %s' % (c, lineno))
             pos += 1
 
+# This AST hierarchy is used to represent the parsed ASDL file as a tree. It's
+# an abstract representation of the grammar in Figure 2 of the ASDL Zephyr
+# paper. Taken from Python's Parser/asdl.py and adapted a bit for 3.4+.
+
+builtin_types = ("identifier", "string", "bytes", "int", "object", "singleton")
+
+class AST:
+    pass # a marker class
+
+class Module(AST):
+    def __init__(self, name, dfns):
+        self.name = name
+        self.dfns = dfns
+        self.types = {} # maps type name to value (from dfns)
+        for type in dfns:
+            self.types[type.name.value] = type.value
+
+    def __repr__(self):
+        return "Module(%s, %s)" % (self.name, self.dfns)
+
+class Type(AST):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __repr__(self):
+        return "Type(%s, %s)" % (self.name, self.value)
+
+class Constructor(AST):
+    def __init__(self, name, fields=None):
+        self.name = name
+        self.fields = fields or []
+
+    def __repr__(self):
+        return "Constructor(%s, %s)" % (self.name, self.fields)
+
+class Field(AST):
+    def __init__(self, type, name=None, seq=False, opt=False):
+        self.type = type
+        self.name = name
+        self.seq = seq
+        self.opt = opt
+
+    def __repr__(self):
+        if self.seq:
+            extra = ", seq=True"
+        elif self.opt:
+            extra = ", opt=True"
+        else:
+            extra = ""
+        if self.name is None:
+            return "Field(%s%s)" % (self.type, extra)
+        else:
+            return "Field(%s, %s%s)" % (self.type, self.name, extra)
+
+class Sum(AST):
+    def __init__(self, types, attributes=None):
+        self.types = types
+        self.attributes = attributes or []
+
+    def __repr__(self):
+        if self.attributes is None:
+            return "Sum(%s)" % self.types
+        else:
+            return "Sum(%s, %s)" % (self.types, self.attributes)
+
+class Product(AST):
+    def __init__(self, fields, attributes=None):
+        self.fields = fields
+        self.attributes = attributes or []
+
+    def __repr__(self):
+        if self.attributes is None:
+            return "Product(%s)" % self.fields
+        else:
+            return "Product(%s, %s)" % (self.fields, self.attributes)
 
 if __name__ == '__main__':
     buf = '''
@@ -70,6 +146,8 @@ if __name__ == '__main__':
             | attributes (int kwa, foo bar)
             --kowo     '''
 
+    import sys
+    buf = open(sys.argv[1]).read()
     for t in tokenize_asdl(buf):
         print(t)
 
