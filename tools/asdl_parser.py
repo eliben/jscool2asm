c4c5991a796237a002f971ea902f2b836d1f175f
Eli Bendersky <eliben@gmail.com>
2013-09-28 15:43:07 -0700

    moar code

--- tools/asdl_parser.py
+++ tools/asdl_parser.py
@@ -4,7 +4,7 @@ from enum import Enum
 import re
 
 TokenKind = Enum('TokenKind',
-    '''ConstructorId TypeId 
+    '''ConstructorId TypeId
        Attributes Module
        Equals Question Pipe LParen RParen Comma Asterisk LBrace RBrace''')
 
@@ -78,7 +78,7 @@ def tokenize_asdl(buf):
 # non-terminals. Id is either TokenId or ConstructorId.
 #
 # module        ::= Id Id "{" [definitions] "}"
-# definitions   ::= { TypeId "=" type
+# definitions   ::= { TypeId "=" type }
 # type          ::= product | sum
 # product       ::= fields
 # fields        ::= "(" { field, "," } field ")"
@@ -89,35 +89,108 @@ def tokenize_asdl(buf):
 class ASDLParser:
     def __init__(self):
         self._tokenizer = None
+        self.cur_token = None
 
     def parse(self, buf):
         """ Parse the ASDL in the buffer and return an AST with a Module root.
         """
         self._tokenizer = tokenize_asdl(buf)
-        while True:
-            self._get_next_token()
-            print(self.cur_token)
-            if self.cur_token is None:
-                print('--done--')
-                return
+        self._advance()
+        return self._parse_module()
 
-    def _get_next_token(self):
+    def _parse_module(self):
+        self._match(TokenKind.Module)
+        name = self._match(self._id_kinds)
+        self._match(TokenKind.LBrace)
+        defs = self._parse_definitions()
+        self._match(TokenKind.RBrace)
+        return Module(name, defs)
+
+    def _parse_definitions(self):
+        defs = []
+        while self.cur_token.kind == TokenKind.TypeId:
+            typename = self._advance()
+            self._match(TokenKind.Equals)
+            type = self._parse_type()
+            defs.append(Type(typename, type))
+        return defs
+
+    def _parse_type(self):
+        if self.cur_token.kind == TokenKind.LParen:
+            # If we see a (, it's a product
+            return Product(self._parse_fields())
+        else:
+            # Otherwise it's a sum. Look for ConstructorId
+            sumlist = [Constructor(self._match(TokenKind.ConstructorId),
+                                   self._parse_optional_fields())]
+            while True:
+                if self.cur_token.kind == TokenKind.Pipe:
+                    # More constructors
+                    self._advance()
+                    sumlist.append(Constructor(
+                                    self._match(TokenKind.ConstructorId),
+                                    self._parse_optional_fields()))
+                elif self.cur_token.kind == TokenKind.Attributes:
+
+
+    def _parse_fields(self):
+        fields = []
+        self._match(TokenKind.LParen)
+        while self.cur_token.kind == TokenKind.TypeId:
+            typename = self._match(TokenKind.TypeId)
+            is_seq, is_opt = self._parse_optional_field_quantifier()
+            id = self._advance() if self.cur_token.kind in self._id_kinds
+                                 else None
+            fields.append(Field(typename id, seq=is_seq, opt=is_opt))
+            if self.cur_token.kind == TokenKind.RParen:
+                break
+            elif self.cur_token.kind == TokenKind.Comma:
+                self._advance()
+        self._match(TokenKind.RParen)
+        return fields
+
+    def _parse_optional_fields(self):
+        if self.cur_token.kind == TokenKind.LParen:
+            return self._parse_fields()
+        else:
+            return None
+
+    def _parse_optional_field_quantifier(self):
+        is_seq, is_opt= False, False
+        if self.cur_token.kind == TokenKind.Asterisk:
+            is_seq= True
+            self._advance()
+        elif self.cur_token.kind == TokenKind.Question:
+            is_opt= True
+            self._advance()
+        return is_seq, is_opt
+
+    def _advance(self):
+        """ Return the value of the current token and read the next one into
+            self.cur_token.
+        """
+        cur_val = None if self.cur_token is None else self.cur_token.value
         try:
             self.cur_token = next(self._tokenizer)
         except StopIteration:
             self.cur_token = None
-        # propagate ASDLSyntaxError from the tokenizer up
+        return cur_val
+
+    _id_kinds = (TokenKind.ConstructorId, TokenKind.TypeId)
 
     def _match(self, kind):
         """ The 'match' primitive of RD parsers.
-            * Verifies that the current token is of the given kind
+            * Verifies that the current token is of the given kind (kind can
+              be a tuple, in which the kind must match one of its members).
             * Returns the value of the current token
             * Reads in the next token
         """
-        if self.cur_token.kind == kind:
-            val = self.cur_token.val
-            self._get_next_token()
-            return val
+        if (isinstance(kind, tuple) and self.cur_token.kind in kind or
+            self.cur_token.kind == kind
+            ):
+            value = self.cur_token.value
+            self._advance()
+            return value
         else:
             raise ASDLSyntaxError('Unmatched %s (found %s)' % (
                 kind, self.cur_token.kind), self.cur_token.lineno)
@@ -209,7 +282,8 @@ if __name__ == '__main__':
             --kowo     '''
 
     import sys
-    buf = open(sys.argv[1]).read()
+    with open(sys.argv[1]) as f:
+        buf = f.read()
     p = ASDLParser()
     p.parse(buf)
 
