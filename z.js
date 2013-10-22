b18ac000c5a4f6f4d8da77d8a559a86520fb0d0f
Eli Bendersky <eliben@gmail.com>
2013-10-22 07:22:02 -0700

    Scribbled down some prototyping of AST nodes with type checking in the constructor and a silly implementation of 'children' that could be used for generic visiting. Next step would be to autogenerate something similar from the ASDL.

--- z.js
+++ z.js
@@ -1,9 +1,69 @@
 'use strict';
 
+function ASTError(message) {
+  this.message = message;
+}
 
-Feature = function() {
-};
+ASTError.prototype = new Error();
+ASTError.prototype.constructor = ASTError;
 
-Method = function(name, formals, return_type, expr) {
-  d
-};
+var _check_string = function(v, who, what) {
+  if (Object.prototype.toString.call(myvar) !== '[object String]') {
+    throw ASTError(who + ' expects ' + what + ' to be a string');
+  }
+}
+
+var _check_array = function(v, who, what) {
+  if (Object.prototype.toString.call(myvar) !== '[object Array]') {
+    throw ASTError(who + ' expects ' + what + ' to be a array');
+  }
+}
+
+var Node = function() {
+}
+
+var Expression = function() {
+}
+
+Expression.prototype = Object.create(Node.prototype);
+Expression.prototype.constructor = Expression;
+
+var Feature = function() {
+}
+
+Feature.prototype = Object.create(Node.prototype);
+Feature.prototype.constructor = Feature;
+
+// Method is a subclass of Feature
+var Method = function(name, formals, return_type, expr, loc) {
+  _check_string(name);
+  this.name = name;
+
+  _check_array(formals);
+  for (var i = 0; i < formals.length; i++) {
+    if (!(formals[i] instanceof Formal)) {
+      throw ASTError('Method expects formals to be an array of Formal');
+    }
+  }
+  this.formals = formals;
+
+  _check_string(return_type);
+  this.return_type = return_type;
+
+  if (!(expr instanceof Expression)) {
+    throw ASTError('Method expects expr to be a Expression');
+  }
+  this.expr = expr;
+  this.loc = loc;
+}
+
+Method.prototype = Object.create(Feature.prototype);
+Method.prototype.constructor = Method;
+
+Method.prototype.children = function() {
+  var nodelist = [];
+  nodelist.push.apply(nodelist, this.formals);
+  nodelist.push(this.expr);
+  return nodelist;
+}
+
