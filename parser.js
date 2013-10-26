6810dbce931975304efae4a3d339c68c1919fd6e
Eli Bendersky <eliben@gmail.com>
2013-10-26 15:03:31 -0700

    cleanup ebnf definition

--- parser.js
+++ parser.js
@@ -15,36 +15,28 @@
 //
 // program       ::= [class;]+
 // 
-// class         ::= CLASS TYPE [INHERITS TYPE] { [feature;]∗ }
+// class         ::= CLASS TYPE [INHERITS TYPE] { [feature ;]∗ }
 // 
-// feature       ::= ID( [formal [, formal]∗] ) : TYPE { expr }
+// feature       ::= ID ( [formal [, formal]∗] ) : TYPE { expr }
 //               |   ID : TYPE [ <- expr ]
 // 
 // formal        ::= ID : TYPE
 // 
 // expr          ::= ID <- expr
-//               |   expr[@TYPE].ID( [expr [, expr]∗] )
-//               |   ID( [expr [, expr]*] )
+//               |   expr[@TYPE].ID ( [expr [, expr]∗] )
+//               |   ID ( [expr [, expr]*] )
 //               |   IF expr THEN expr ELSE expr FI
 //               |   WHILE expr LOOP expr POOL
 //               |   { [expr; ]+ }
-//               |   LET ID : TYPE [<- expr] [, ID : TYPE [ <- expr] ]* IN expr
-//               |   CASE expr OF [ID : TYPE => expr;]+ ESAC
+//               |   LET ID : TYPE [<- expr] [, ID : TYPE [ <- expr]]* IN expr
+//               |   CASE expr OF [ID : TYPE => expr ;]+ ESAC
 //               |   NEW TYPE
 //               |   ISVOID expr
-//               |   expr + expr
-//               |   expr − expr
-//               |   expr ∗ expr
-//               |   expr / expr
-//               |   ~expr
-//               |   expr < expr
-//               |   expr <= expr
-//               |   expr = expr
+//               |   expr + expr | expr − expr
+//               |   expr ∗ expr | expr / expr
+//               |   expr < expr | expr <= expr
+//               |   expr = expr | ~ expr
 //               |   NOT expr
-//               |   (expr)
-//               |   ID
-//               |   NUMBER
-//               |   STRING
-//               |   true
-//               |   false
+//               |   ( expr )
+//               |   ID | NUMBER | STRING | TRUE | FALSE
 
