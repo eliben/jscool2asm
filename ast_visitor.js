76cc0301bef7d88e190b9bd5d75ecb0ae2922574
Eli Bendersky <eliben@gmail.com>
2013-10-30 06:09:07 -0700

    Fill in implementation of Nodevisitor.prototype.visit [untested]

--- ast_visitor.js
+++ ast_visitor.js
@@ -6,7 +6,10 @@ var NodeVisitor = exports.NodeVisitor = function() {
 
 
 NodeVisitor.prototype.visit = function(node) {
-  
+  var visitor_name = 'visit_' + node.constructor.node_type;
+  var method = (visitor_name in this) ? this['visitor_name']
+                                      : this._generic_visit;
+  return method.call(this, node);
 }
 
 
