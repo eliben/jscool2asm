1a94875e6172832b63b038f64a1d9fea29445e3b
Eli Bendersky <eliben@gmail.com>
2013-10-30 07:12:46 -0700

    Debugged visitor with tests

--- ast_visitor.js
+++ ast_visitor.js
@@ -1,18 +1,25 @@
 'use strict';
 
 
+// NodeVisitor - implements the classical visitor design pattern.
+//
+// To create custom visitors, inherit from NodeVisitor and define methods
+// visit_Type for visiting nodes of type Type. Custom visitors inherit the
+// visit_children method which can be used to generically dispatch children
+// nodes if the custom visitor doesn't have special provision for them.
 var NodeVisitor = exports.NodeVisitor = function() {
 }
 
-
 NodeVisitor.prototype.visit = function(node) {
   var visitor_name = 'visit_' + node.constructor.node_type;
-  var method = (visitor_name in this) ? this['visitor_name']
-                                      : this._generic_visit;
+  var method = (visitor_name in this) ? this[visitor_name]
+                                      : this.visit_children;
   return method.call(this, node);
 }
 
-
-NodeVisitor.prototype._generic_visit = function(node) {
-  
+NodeVisitor.prototype.visit_children = function(node) {
+  var children = node.children();
+  for (var i = 0; i < children.length; i++) {
+    this.visit(children[i].node);
+  }
 }
--- test/test_ast.js
+++ test/test_ast.js
@@ -4,31 +4,18 @@
 
 var assert = require('assert');
 var ast = require('../cool_ast');
+var ast_visitor = require('../ast_visitor');
+
 
 var test = function() {
-  // Success tests - objects created successfully, etc.
+  basic_tests();
+  error_tests();
+  visitor_tests();
+}
+
+var error_tests = function() {
   var obj = new ast.Obj('foo', 100);
-  assert(obj instanceof ast.Expression);
-  assert(obj instanceof ast.Node);
-  assert.equal(obj.name, 'foo');
-  assert.equal(obj.loc, 100);
-
   var form = new ast.Formal('oshu', 'java');
-  assert(form instanceof ast.Formal);
-  assert(form instanceof ast.Node);
-  assert.equal(form.name, 'oshu');
-  assert.equal(form.type_decl, 'java');
-
-  var meth = new ast.Method('fibo', [form], 'int', obj);
-  assert(meth instanceof ast.Feature);
-  assert(meth instanceof ast.Node);
-  assert.deepEqual(meth.constructor.attributes, ['name', 'return_type']);
-  assert.equal(meth.constructor.node_type, 'Method');
-  var children = meth.children();
-  console.log(children[0]);
-  assert.equal(children[0].name, 'formals[0]');
-  assert.equal(children[0].node, form);
-
   // Failure tests - trigger some errors ASTs constructors can catch.
   assert.throws(
     function() {
@@ -57,6 +44,58 @@ var test = function() {
       var meth2 = new ast.Method('fibo', [form], 'int', form);
     },
     ast.ASTError);
+}
+
+var basic_tests = function() {
+  // Success tests - objects created successfully, etc.
+  var obj = new ast.Obj('foo', 100);
+  assert(obj instanceof ast.Expression);
+  assert(obj instanceof ast.Node);
+  assert.equal(obj.name, 'foo');
+  assert.equal(obj.loc, 100);
+
+  var form = new ast.Formal('oshu', 'java');
+  assert(form instanceof ast.Formal);
+  assert(form instanceof ast.Node);
+  assert.equal(form.name, 'oshu');
+  assert.equal(form.type_decl, 'java');
+
+  var meth = new ast.Method('fibo', [form], 'int', obj);
+  assert(meth instanceof ast.Feature);
+  assert(meth instanceof ast.Node);
+  assert.deepEqual(meth.constructor.attributes, ['name', 'return_type']);
+  assert.equal(meth.constructor.node_type, 'Method');
+  var children = meth.children();
+  assert.equal(children[0].name, 'formals[0]');
+  assert.equal(children[0].node, form);
+}
+
+// Used for testing NodeVisitor
+var CustomVisitor = function() {
+  this.stuff = [];
+}
+
+CustomVisitor.prototype = Object.create(ast_visitor.NodeVisitor.prototype);
+CustomVisitor.constructor = CustomVisitor;
+
+CustomVisitor.prototype.visit_Attr = function(node) {
+  this.stuff.push(['attr', node.name]);
+  this.visit_children(node);
+}
+
+CustomVisitor.prototype.visit_New = function(node) {
+  this.stuff.push(['new', node.type_name]);
+}
+
+var visitor_tests = function() {
+  var attr1 = new ast.Attr('attr1', 'f', new ast.New('int'));
+  var attr2 = new ast.Attr('attr2', 'ff', new ast.NoExpr());
+  var cls = new ast.Class('c', 'p', [attr1, attr2], 'f');
+
+  var cv = new CustomVisitor();
+  cv.visit(cls);
+  assert.deepEqual(cv.stuff,
+      [['attr', 'attr1'], ['new', 'int'], ['attr', 'attr2']]);
 }
 
 if (module.parent === null) {
