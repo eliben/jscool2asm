f1029b9ab2e8aa1631689e5e02b99494287513c7
Eli Bendersky <eliben@gmail.com>
2013-11-05 06:59:27 -0800

    Class-level tests

--- test/test_parser.js
+++ test/test_parser.js
@@ -10,6 +10,7 @@ var ast_visitor = require('../ast_visitor');
 
 var test = function() {
   test_program();
+  test_class();
 }
 
 var parse = function(s) {
@@ -37,6 +38,37 @@ var test_program = function() {
   assert.equal(prog.classes[0].loc, 2);
   assert.equal(prog.classes[1].name, 'C2');
   assert.equal(prog.classes[1].loc, 4);
+
+  var _make_class = function(n) {
+    return 'class C' + n + '{x : Y}\n\n';
+  }
+
+  s = '';
+  for (var i = 0; i < 100; i++) {
+    s += _make_class(i);
+  }
+  prog = parse(s);
+  assert.equal(prog.classes.length, 100);
+  assert.equal(prog.classes[99].loc, 199);
+}
+
+var test_class = function() {
+  var _parse_class0 = function(s) {
+    return parse(s).classes[0];
+  }
+
+  // Class-level nodes
+  var cls = _parse_class0('class C {at : Int}');
+  assert.equal(cls.name, 'C');
+  assert.equal(cls.parent, null);
+  assert.equal(cls.features.length, 1);
+
+  cls = _parse_class0('class C {at : Int; f : F}');
+  assert.equal(cls.features.length, 2);
+
+  cls = _parse_class0('class F inherits T {at : Int}');
+  assert.equal(cls.name, 'F');
+  assert.equal(cls.parent, 'T');
 }
 
 if (module.parent === null) {
